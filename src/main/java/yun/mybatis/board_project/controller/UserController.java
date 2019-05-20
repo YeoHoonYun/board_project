e21bb112b6db824bcb637e2af02e9a1f6f5cebf7
YunYeoHoon <yun9207@jiguem.com>
2019-05-20 14:28:44 +0900

    CREATE 기능 구현

--- src/main/java/yun/mybatis/board_project/controller/UserController.java
+++ src/main/java/yun/mybatis/board_project/controller/UserController.java
@@ -1,4 +1,34 @@
 package yun.mybatis.board_project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import yun.mybatis.board_project.service.UserService;
+
+@Controller
 public class UserController {
+    @Autowired
+    UserService userService;
+
+    @GetMapping(value = {"/login"})
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping(value = {"/join"})
+    public String join(){
+        return "join";
+    }
+
+    @GetMapping(value = {"/login"})
+    public String userLogin(){
+        return "join";
+    }
+
+    @GetMapping(value = {"/join"})
+    public String userJoin(){
+        return "login";
+    }
+
 }
